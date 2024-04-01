8166605700dc1b49d908214b8e0789de8883be28
sakovolga <sakovolga@live.com>
2024-04-01 15:40:57 +0200

    refactoring: move creating expectedUser to ExpectedData class

--- src/test/java/com/example/online_toy_store/controller/UserControllerTest.java
+++ src/test/java/com/example/online_toy_store/controller/UserControllerTest.java
@@ -4,6 +4,7 @@ import com.example.online_toy_store.entity.*;
 import com.example.online_toy_store.entity.enums.City;
 import com.example.online_toy_store.entity.enums.Country;
 import com.example.online_toy_store.exception.UserDoesNotExistException;
+import com.example.online_toy_store.utils.ExpectedData;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -38,50 +39,7 @@ public class UserControllerTest {
     @Test
     void showUserTestPositive() throws Exception {
 
-        User user = new User();
-        user.setUID(UUID.fromString("1b4a432d-1ec9-4141-ace1-1d6ed2e3de0f"));
-        user.setFirstName("Julija");
-        user.setLastName("Klimenko");
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime dateTime = LocalDateTime.parse("2024-01-07 12:20:49", formatter);
-        user.setCreatedAt(dateTime);
-        user.setCountry(Country.GERMANY);
-
-        UserInfo userInfo = new UserInfo();
-        userInfo.setUID(UUID.fromString("c9fb87a6-b0ff-457b-a646-6b4a2a107396"));
-        userInfo.setUserName("julija_klimenko");
-        userInfo.setAddress("Rosenweg 7");
-        userInfo.setCity(City.DREZDEN);
-        userInfo.setPostalCode("32546");
-        userInfo.setEmail("klim@gmail.com");
-        userInfo.setPassword("765000!fdbvytr");
-        userInfo.setCardNumber("3463 8895 2271 479");
-        user.setUserInfo(userInfo);
-
-        Role role1 = new Role();
-        role1.setRId(UUID.fromString("e8e47c95-84bb-4262-819f-7eace92d5b7b"));
-        role1.setRoleName("customer");
-        Set<Role> roleSet = Set.of(role1);
-        userInfo.setRoles(roleSet);
-
-        Authority authority1 = new Authority();
-        authority1.setAId(UUID.fromString("1a10d548-3cf1-4c7c-9823-5e30cf273ad4"));
-        authority1.setAuthorityName("Update customer profile");
-
-        Authority authority2 = new Authority();
-        authority2.setAId(UUID.fromString("a6d7c833-0fbf-4bdc-b07c-b3bbd3d8d708"));
-        authority2.setAuthorityName("Place order");
-
-        Authority authority3 = new Authority();
-        authority3.setAId(UUID.fromString("b30fdaec-15f4-4df4-a2d4-cd4907f94a9a"));
-        authority3.setAuthorityName("Create customer profile");
-
-        Authority authority4 = new Authority();
-        authority4.setAId(UUID.fromString("ff6b1bc1-1d0c-4f0f-b3e7-8be9ae76b986"));
-        authority4.setAuthorityName("View own orders");
-
-        Set<Authority> authoritySet = Set.of(authority1, authority2, authority3, authority4);
-        role1.setAuthorities(authoritySet);
+        User user = ExpectedData.returnUser();
 
         MvcResult showUserResult =
                 mockMvc
--- src/test/java/com/example/online_toy_store/utils/ExpectedData.java
+++ src/test/java/com/example/online_toy_store/utils/ExpectedData.java
@@ -1,9 +1,6 @@
 package com.example.online_toy_store.utils;
 
-import com.example.online_toy_store.entity.Order;
-import com.example.online_toy_store.entity.Product;
-import com.example.online_toy_store.entity.Review;
-import com.example.online_toy_store.entity.Supplier;
+import com.example.online_toy_store.entity.*;
 import com.example.online_toy_store.entity.enums.City;
 import com.example.online_toy_store.entity.enums.Country;
 
@@ -101,5 +98,52 @@ public class ExpectedData {
         return Set.of(order1, order2, order3, order4, order5);
     }
 
+    public static User returnUser(){
+        User user = new User();
+        user.setUID(UUID.fromString("1b4a432d-1ec9-4141-ace1-1d6ed2e3de0f"));
+        user.setFirstName("Julija");
+        user.setLastName("Klimenko");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime dateTime = LocalDateTime.parse("2024-01-07 12:20:49", formatter);
+        user.setCreatedAt(dateTime);
+        user.setCountry(Country.GERMANY);
 
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUID(UUID.fromString("c9fb87a6-b0ff-457b-a646-6b4a2a107396"));
+        userInfo.setUserName("julija_klimenko");
+        userInfo.setAddress("Rosenweg 7");
+        userInfo.setCity(City.DREZDEN);
+        userInfo.setPostalCode("32546");
+        userInfo.setEmail("klim@gmail.com");
+        userInfo.setPassword("765000!fdbvytr");
+        userInfo.setCardNumber("3463 8895 2271 479");
+        user.setUserInfo(userInfo);
+
+        Role role1 = new Role();
+        role1.setRId(UUID.fromString("e8e47c95-84bb-4262-819f-7eace92d5b7b"));
+        role1.setRoleName("customer");
+        Set<Role> roleSet = Set.of(role1);
+        userInfo.setRoles(roleSet);
+
+        Authority authority1 = new Authority();
+        authority1.setAId(UUID.fromString("1a10d548-3cf1-4c7c-9823-5e30cf273ad4"));
+        authority1.setAuthorityName("Update customer profile");
+
+        Authority authority2 = new Authority();
+        authority2.setAId(UUID.fromString("a6d7c833-0fbf-4bdc-b07c-b3bbd3d8d708"));
+        authority2.setAuthorityName("Place order");
+
+        Authority authority3 = new Authority();
+        authority3.setAId(UUID.fromString("b30fdaec-15f4-4df4-a2d4-cd4907f94a9a"));
+        authority3.setAuthorityName("Create customer profile");
+
+        Authority authority4 = new Authority();
+        authority4.setAId(UUID.fromString("ff6b1bc1-1d0c-4f0f-b3e7-8be9ae76b986"));
+        authority4.setAuthorityName("View own orders");
+
+        Set<Authority> authoritySet = Set.of(authority1, authority2, authority3, authority4);
+        role1.setAuthorities(authoritySet);
+
+        return user;
+    }
 }
